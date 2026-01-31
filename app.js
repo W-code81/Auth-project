5f823552ed9d8249f594564a0be283af8b17406f
oluwadamilolaaderogba@gmail.com <oluwadamilolaaderogba@gmail.com>
2026-01-31 02:18:48 +0100

    passport.js auth

--- app.js
+++ app.js
@@ -3,12 +3,25 @@ const app = express();
 const port = process.env.PORT || 3000;
 require("dotenv").config();
 const validator = require("validator");
-const bcrypt = require("bcrypt");
-const saltRounds = 10;
+const session = require("express-session");
+const passport = require("passport");
+const passportLocalMongoose = require("passport-local-mongoose").default;
+
 
 app.set("view engine", "ejs");
 app.use(express.urlencoded({ extended: true }));
 app.use(express.static("public"));
+
+app.use(
+  session({
+    secret: process.env.SESSION_SECRET,
+    resave: false, // doesn't save unchanged sessions
+    saveUninitialized: false, // doesn't create if empty
+  })
+);
+
+app.use(passport.initialize()); //initializes passport
+app.use(passport.session()); //manages persistent login sessions
 
 const mongoose = require("mongoose");
 
@@ -21,18 +34,14 @@ const userSchema = mongoose.Schema(
   {
     email: {
       type: String,
-      required: [ true, "Email is required" ],
+      required: [true, "Email is required"],
       unique: true,
       trim: true,
       lowercase: true, //to ensure email consistency
-      validate:{
+      validate: {
         validator: validator.isEmail,
-        message: "Please enter a valid email"
-      }
-    },
-    password: {
-      type: String,
-      required: true,
+        message: "Please enter a valid email",
+      },
     },
   },
   {
@@ -40,8 +49,13 @@ const userSchema = mongoose.Schema(
   }
 );
 
+userSchema.plugin(passportLocalMongoose, { usernameField: "email" }); // enabled to hash and salt paasswords and save users into the db. also uses email as username
 
 const User = mongoose.model("User", userSchema);
+
+passport.use(User.createStrategy()); //passport local strategy - auth user using their username and password
+passport.serializeUser(User.serializeUser()); //creates a cookie and stores users info
+passport.deserializeUser(User.deserializeUser()); //removes cookie and retrieves users info 
 
 app.get("/", (req, res) => {
   res.render("home");
@@ -57,15 +71,12 @@ app
     try {
       const { email, password } = req.body;
 
-      //secure hashing
-      const hashedPassword = await bcrypt.hash(password, saltRounds);
+      const newUser = await User.register({ email: email }, password); //registers user and hashes password
 
-      const newUser = await User.create({
-        email: email,
-        password: hashedPassword,
+      passport.authenticate("local")(req, res, () => { //authenticates user using local strategy if registration is successful and redirects to secrets page
+        res.redirect("/secrets");
       });
-
-        res.render("secrets");
+     
     } catch (err) {
       if (err.code === 11000) {
         return res.status(400).send("Email already registered");
@@ -73,6 +84,14 @@ app
       res.status(500).send("Registration error");
     }
   });
+
+app.get("/secrets", (req, res) => { 
+  if (req.isAuthenticated()) {
+    res.render("secrets"); //renders secrets page if user is authenticated
+  } else {
+    res.redirect("/login");
+  }
+});
 
 app
   .route("/login")
@@ -85,16 +104,12 @@ app
     try {
       const { email, password } = req.body;
 
-    //checking for the user with the particular email and validating
-      const user = await User.findOne({ email: email });
-      if (!user) return res.status(401).send("invalid credentials");
+     const user = await User.create({
+      email: email,
+      password: password
+     });
 
-      //using bcrypt to validate if the hashed passwords are the same
-      const isValid = await bcrypt.compare(password , user.password);
-      if (!isValid) return res.status(401).send("invalid credentials");
-
-      res.render("secrets");
-
+      // res.render("secrets");
     } catch (err) {
       res.status(500).send("login server error");
     }
